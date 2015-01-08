73ee4274424e6cd4c5363e4823e43f3e3db9c205
RAM0N-LUIS <ramonlrodriguez@gmail.com>
2015-01-07 20:39:02 -0600

    updated markdown logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,12 +7,4 @@ module ApplicationHelper
 			'form-group'
 		end
 	end
-
-	def markdown(text)
-		renderer = Redcarpet::Render::HTML.new
-		extensions = {fenced_code_blocks: true, autolink: true, strikethrough: true, quote: true}
-		redcarpet = Redcarpet::Markdown.new(renderer,extensions)
-		(redcarpet.render text).html_safe
-	end
-
 end
