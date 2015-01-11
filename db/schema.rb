47cf651addc6ba0feb91c357ac46aed8c4f7216b
RAM0N-LUIS <ramonlrodriguez@gmail.com>
2015-01-11 17:35:58 -0600

    added image uploader to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109034748) do
+ActiveRecord::Schema.define(version: 20150111232349) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150109034748) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.string   "image"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
@@ -40,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150109034748) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "users", force: true do |t|
