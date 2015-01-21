613fc8a580ce715c406d344bc3cb8a7807fc840c
RAM0N-LUIS <ramonlrodriguez@gmail.com>
2015-01-20 21:22:10 -0600

    updated routes with shallow nesting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
           
   def create
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    @topic = @post.topic
+    @post = posts.find(params[:post_id])
     @comment = current_user.comments.build(params.require(:comment).permit(:body))
     @comment.post = @post
     @comments = @post.comments
@@ -11,15 +11,15 @@ class CommentsController < ApplicationController
     
     if @comment.save
       flash[:notice] = "Comment added."
-      redirect_to [@topic, @post]
+      redirect_to @post
     else
       flash[:error] = "There was an error adding your comment. Please try again."
     end
   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    @topic = @post.topic
+    @post = posts.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
 
     authorize @comment
@@ -30,7 +30,7 @@ class CommentsController < ApplicationController
       flash[:notice] = "Comment couldn't be deleted.  Try again."
     end
 
-    redirect_to [@topic, @post]
+    redirect_to @post
   
   end
 
