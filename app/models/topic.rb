989eafce404833837f2a15c18a9bdf7bd008f6f2
RAM0N-LUIS <ramonlrodriguez@gmail.com>
2015-01-28 22:04:01 -0500

    public and private views

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,7 +5,7 @@ class Topic < ActiveRecord::Base
   scope :publicly_viewable, -> { where(public: true) }
   scope :privately_viewable, -> { where(public: false) }
 
-  scope :visible_to, -> (user) { user ? all : where(publicly_viewable: true) }
+  scope :visible_to, -> (user) { user ? publicly_viewable | privately_viewable : publicly_viewable }
 
 
 
