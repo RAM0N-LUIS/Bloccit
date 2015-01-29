79200f0a72f66bcb0eacc1edca6abc91f51122f0
RAM0N-LUIS <ramonlrodriguez@gmail.com>
2015-01-28 21:34:03 -0500

    private topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,12 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+
+ 
+  scope :publicly_viewable, -> { where(public: true) }
+  scope :privately_viewable, -> { where(public: false) }
+
+  scope :visible_to, -> (user) { user ? all : where(publicly_viewable: true) }
+
+
+
 end
