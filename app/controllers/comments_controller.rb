4f1fa2f24dcdb9223ffcd770e29f65eedb557d20
RAM0N-LUIS <ramonlrodriguez@gmail.com>
2015-02-07 18:55:19 -0600

    create comments AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,19 +4,24 @@ class CommentsController < ApplicationController
   def create
 
     @post = Post.find(params[:post_id])
-    @topic = @post.topic
+    @comments = @post.comments
     @comment = current_user.comments.build(params.require(:comment).permit(:body))
     @comment.post = @post
-    @comments = @post.comments
+    @new_comment = Comment.new
+    
     
     authorize @comment
     
     if @comment.save
       flash[:notice] = "Comment added."
-      redirect_to [@topic, @post]
     else
-      flash[:error] = "There was an error adding your comment. Please try again."
+      flash[:error] = "There was an error saving your comment. Please try again."
     end
+
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post] }
+    end
+
   end
 
   def destroy
